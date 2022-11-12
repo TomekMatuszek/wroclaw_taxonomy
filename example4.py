7ed15206eb48dccb3d2c0d87415a3b51820b65ef
Tomek Matuszek <tom.mateuszek@gmail.com>
2022-11-12 03:01:41 +0100

    merging examples

--- example4.py
+++
@@ -1,9 +0,0 @@
-from wroclaw_taxonomy.wroclaw_taxonomy import create_dendrite
-
-create_dendrite(
-    in_file='data/powiaty_dane.geojson',
-    columns=['bezrobocie-TABLICA_[%]', 'stan ludności-TABLICA_[osoba]', 'wynagrodzenia-TABLICA_[%]'],
-    normalize=True,
-    out_file='dendrite_points2.geojson',
-    type='points'
-)
