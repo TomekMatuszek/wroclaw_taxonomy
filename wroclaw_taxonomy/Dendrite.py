1b6af1c6397e6831d4f5ac1d89f610cfc04d9755
Tomek Matuszek <tom.mateuszek@gmail.com>
2022-11-12 12:14:57 +0100

    english comments

--- wroclaw_taxonomy/Dendrite.py
+++ wroclaw_taxonomy/Dendrite.py
@@ -49,7 +49,7 @@ class Dendrite:
     def calculate(self, columns=['lat', 'lon'], normalize=False):
         data = self.data
         assert isinstance(columns, list), 'Argument columns has to be a list'
-        # stworzenie macierzy odległości
+        # create distance matrix
         if normalize == True:
             if any(item in ('lat', 'lon') for item in columns):
                 warnings.warn('You are normalizing coordinate values. It may slightly change results.')
@@ -60,31 +60,30 @@ class Dendrite:
         distance_matrix = np.array(cdist(for_matrix, for_matrix, metric='euclidean'))
         self.matrix = np.array(cdist(for_matrix, for_matrix, metric='euclidean'))
         
-        # wyznaczenie najbliższych sąsiadów
+        # get nearest neighbours
         data['nearest1'] = np.argmin(ma.masked_array(distance_matrix, mask= distance_matrix==0), axis=1) + 1
         data['cluster1'] = np.argmin(ma.masked_array(distance_matrix, mask= distance_matrix==0), axis=1) + 1
 
-        # zgrupowanie punktów w klastry
+        # grouping into clusters
         for i in range(0, data.shape[0]):
             data.loc[data['cluster1'] == i + 1, 'cluster1'] = data.loc[i, 'cluster1']
 
-        # czyszczenie macierzy ze stworzonych połączeń
+        # clearing matrix
         distance_matrix = self.__clear_matrix(data, distance_matrix, 'cluster1')
 
-        # powtarzanie łączenia aż do otrzymania jednego klastra
+        # repeating clustering to get one big cluster
         lvl = 2
         while data[f'cluster{lvl-1}'].unique().shape[0] > 1:
-            # wyznaczenie najbliższego punktu wraz z odległością po maskowaniu macierzy z zer
+            # get nearest neighbour of cluster
             data[f'nearest{lvl}'] = np.argmin(ma.masked_array(distance_matrix, mask= distance_matrix==0), axis=1) + 1
             data[f'nearest{lvl}_dist'] = np.min(ma.masked_array(distance_matrix, mask= distance_matrix==0), axis=1)
 
-            # pozostawienie tylko najbliższych odległości dla każdego klastra
             for i in data[f'cluster{lvl-1}'].unique():
                 cluster = data.loc[data[f'cluster{lvl-1}'] == i, :]
                 nearest = cluster.loc[cluster[f'nearest{lvl}_dist'] == np.min(cluster[f'nearest{lvl}_dist']), 'fid']
                 data.loc[(data['fid'] != nearest.values[0]) & (data[f'cluster{lvl-1}'] == i), f'nearest{lvl}'] = -1
 
-            # przyporządkowanie numeru klastra do którego należy najbliższy sąsiad
+            # get id of nearest cluster
             for i in range(0, data.shape[0]):
                 if data.loc[i, f'nearest{lvl}'] == -1:
                     continue
@@ -92,16 +91,16 @@ class Dendrite:
                     nearest_cluster = data.loc[data['fid'] == data.loc[i, f'nearest{lvl}'], f'cluster{lvl-1}']
                     data.loc[data[f'cluster{lvl-1}'] == data.loc[i, f'cluster{lvl-1}'], f'cluster{lvl}'] = nearest_cluster.values[0]
 
-            # zgrupowanie klastrów w większe klastry
+            # grouping clusters into bigger ones
             for i in range(0, data.shape[0]):
                 data.loc[data[f'cluster{lvl}'] == data.loc[i, f'cluster{lvl-1}'], f'cluster{lvl}'] = data.loc[i, f'cluster{lvl}']
 
-            # czyszczenie macierzy
+            # clearing distance matrix
             distance_matrix = self.__clear_matrix(data, distance_matrix, f'cluster{lvl}')
             lvl += 1
 
         self.levels = lvl
-        # tworzenie linestringów dla połączeń każdego rzędu
+        # linestrings for every connection level
         for i in range(1, lvl):
             for j in range(0, data.shape[0]):
                 if data.loc[j, f'nearest{i}'] != -1:
@@ -109,6 +108,7 @@ class Dendrite:
                 else:
                     data.loc[j, f'line{i}'] = ''
         
+        # counting connections for every point
         for i in range(0, data.shape[0]):
             to_ids = {x for lst in [data.loc[data[f'nearest{j}'] == i + 1, 'fid'].to_list() for j in range(1, self.levels)] for x in lst}
             from_ids = set([data.loc[i, f'nearest{j}'] for j in range(1, self.levels)]) - {-1}
@@ -133,11 +133,11 @@ class Dendrite:
         return dendrite
     def plot(self):
         dendrite = self.dendrite
-        points = self.results
+        objects = self.results
         fig, ax = plt.subplots(figsize = (10, 10))
         for lvl, lwd in zip(range(1, max(dendrite['level']) + 1), np.arange(0.5, 2 + (1.5 / (max(dendrite['level']) + 1)), (1.5 / (max(dendrite['level']) + 1)))):
             dendrite[dendrite['level'] == lvl].plot(ax=ax, color='#222222',  linewidth=lwd, zorder=5)
 
-        points.plot(ax=ax, color='#ff0000', markersize=(points['connections'] - 0.75) * 2, zorder=10)
+        objects.plot(ax=ax, color='#ff0000', markersize=(objects['connections'] - 0.75) * 2, zorder=10)
         plt.show()
 
